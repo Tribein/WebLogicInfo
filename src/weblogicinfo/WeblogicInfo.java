47281573847ccdad08e7c9ad398804c272d87e83
Tribein <tribein@gmail.com>
2018-09-09 14:32:05 +0300

    Merge origin/master
    Conflicts:
    	clickhouse_schema/sessions.sql

--- src/weblogicinfo/WeblogicInfo.java
+++ src/weblogicinfo/WeblogicInfo.java
@@ -1,7 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 
+ * Copyright (C) 2018 Tribein
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package weblogicinfo;
 
@@ -10,10 +21,6 @@ import org.eclipse.jetty.jmx.MBeanContainer;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletHandler;
 
-/**
- *
- * @author lesha
- */
 public class WeblogicInfo {
 
     /**
